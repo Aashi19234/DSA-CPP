1030a0d8b91829698e4d3cdf5f7a0925374a38b5
Aashi19234 <aasthaj375@gmail.com>
2024-02-10 22:56:29 +0530

    dsa

--- ques.cpp
+++ ques.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int sum=0;
+    /*int sum=0;
     int arr[4]={3,5,6,8};
     for(int i=0;i<4;i++){
         sum=sum+=arr[i];
 
     }
-    cout<<sum;
+    cout<<sum;*/
+    char name[7]="Aastha";
+    int count =0;
+    for(int i=0;name[i]!='\0';i++){
+        count++;
+    }
+    cout<<count;
+
 }
