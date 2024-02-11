44aa53ac1f28cac959efeedc3a00f185f91d83bb
Aashi19234 <aasthaj375@gmail.com>
2024-02-11 21:35:37 +0530

    code

--- ques.cpp
+++ ques.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
+void reverse(char arr[], int n){
+    int s=0;
+    int e=n-1;
+    while(s<e){
+        swap(arr[s],arr[e]);
+        s++;
+        e--;
+    }
+}
 int main(){
     /*int sum=0;
     int arr[4]={3,5,6,8};
@@ -8,11 +17,16 @@ int main(){
 
     }
     cout<<sum;*/
-    char name[7]="Aastha";
+    /*char name[7]="Aastha";
     int count =0;
     for(int i=0;name[i]!='\0';i++){
         count++;
     }
-    cout<<count;
+    cout<<count;*/
+    char arr[7]="Aastha";
+    
+    
+    
+
 
 }
