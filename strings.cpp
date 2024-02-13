f5e466ca2c301b409fa73fd10ed419aadd512d42
Aashi19234 <aasthaj375@gmail.com>
2024-02-13 22:46:24 +0530

    strings

--- strings.cpp
+++ strings.cpp
@@ -1,7 +1,7 @@
 //check palindrome
 #include<iostream>
 using namespace std;
-bool checkPalindrome(char a[], int n){
+/*bool checkPalindrome(char a[], int n){
     int s=0;
     int e=n-1;
     while(s<e){
@@ -20,4 +20,32 @@ int main(){
     char name[7]="Aastha";
     cout<<checkPalindrome(name,7);
     return 0;
-}
+}*/
+//string compression
+class Solution {
+public:
+    int compress(int chars[]) {
+        int i=0;
+        int ansIndex=0;
+        int n;
+     
+        while(i<n){
+            int j=i+1;
+            while(j<n && chars[i]==chars[j]){
+                j++;
+            }
+            chars[ansIndex++]=chars[i];
+            int count=j-i;
+            if(count>1){
+                string ct= to_string(count);
+                for( char ch: ct){
+                    chars[ansIndex++]= ch;
+                }
+            }
+            i=j;
+
+        }
+        return ansIndex;
+        
+    }
+};
