68a421c3817ad5080c5f32071a9be6867d0fccfd
Aashi19234 <aasthaj375@gmail.com>
2024-03-06 20:23:43 +0530

    sorting

--- recursion.cpp
+++ recursion.cpp
@@ -1,6 +1,74 @@
 #include<iostream>
 using namespace std;
-void sortarray(int arr[],int n){
+void merge(int arr[],int s,int e){
+    int mid=s+(e-s)/2;
+    int len1=mid-s+1;
+    int len2=e-mid;
+    int *first =new int[len1];// dynamic memory allocation
+    int *second = new int[len2];
+    // copy values
+    int mainArrayindex=s;
+    for(int i=0;i<len1;i++){
+        first[i]=arr[ mainArrayindex++];
+    }
+     mainArrayindex=mid+1;
+    for(int i=0;i<len2;i++ ){
+        second[i]=arr[ mainArrayindex++];
+    }
+    // merge 2 sorted arrays
+    int index1=0;
+    int index2=0;
+     mainArrayindex=s;
+    while(index1<len1&&index2<len2){
+        if(first[index1]<second[index2]){
+            arr[mainArrayindex++]=first[index1++];
+        }
+        else{
+            arr[mainArrayindex++]=second[index2++];
+        }
+}
+while(index1<len1){
+    arr[mainArrayindex++]=first[index1++];
+}
+while(index2<len2){
+    arr[mainArrayindex++]=second[index2++];
+}
+delete []first;// deletion of dynamically allocated memory
+delete []second;
+
+    
+
+
+
+
+
+
+}
+void mergesort(int arr[],int s,int e){
+    if(s>=e) return;
+    int mid=s+(e-s)/2;
+    //solve left part
+    mergesort(arr,s,mid);
+    // solve right part
+    mergesort(arr,mid+1,e);
+    //merge 
+    merge(arr,s,e);
+}
+
+
+// merge sort using recursion
+int main(){
+    int arr[6]={32,45,23,12,67,43};
+    int n=6;
+    mergesort(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+
+/*void sortarray(int arr[],int n){
     if(n==0||n==1){
         return;
     }
@@ -20,7 +88,7 @@ int main(){
     for(int i=0;i<5;i++){
         cout<<arr[i]<<" ";
     }
-}
+}*/
 
 
 /*int power(int a,int b){
