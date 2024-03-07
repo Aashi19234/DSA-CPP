3e39c14f967f6b49c774ec114d72e7239368fe29
Aashi19234 <aasthaj375@gmail.com>
2024-03-08 00:37:06 +0530

    ..

--- recursion.cpp
+++ recursion.cpp
@@ -1,6 +1,66 @@
 #include<iostream>
 using namespace std;
-void merge(int arr[],int s,int e){
+int partition(int arr[],int s,int e){
+    int pivot=arr[s];
+    int cnt=0;
+    for(int i=s+1;i<=e;i++){
+        if(arr[i]<=pivot){
+            cnt++;
+        }
+    }
+    int pivotindex=s+cnt;
+    // place pivot at right pos
+    swap(arr[pivotindex],arr[s]);
+    int i=s;
+    int j=e;
+    while(i<pivotindex&& j>pivotindex){
+        while(arr[i]<pivot){
+            i++;
+            
+        }
+        while(arr[j]>pivot){
+            j--;
+        }
+        if(i<pivotindex&&j>pivotindex){
+            swap(arr[i++],arr[j--]);
+        }
+
+    }
+    return pivotindex;
+
+}
+void quicksort(int arr[],int s,int e){
+    // base case
+    if(s>=e){
+        return;
+    }
+    // partition 
+    int p=partition(arr,s,e);
+    // leftpart
+    quicksort(arr,s,p-1);
+    // reight part
+    quicksort(arr,p+1,e);
+}
+
+
+// quick sort using recursion
+
+int main(){
+    int arr[5]={2,4,1,6,8};
+    int n=5;
+    quicksort(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+
+    }
+    return 0;
+
+
+
+}
+
+
+/*void merge(int arr[],int s,int e){
     int mid=s+(e-s)/2;
     int len1=mid-s+1;
     int len2=e-mid;
@@ -64,7 +124,7 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-}
+}*/
 
 
 
